585362ca6e7a3b0a30cd101c62e57a87c6ed366a
Richard Kuhnt <r15ch13+git@gmail.com>
2017-06-12 18:06:21 +0200

    Cleanup scoop cache

--- updater.sh
+++ updater.sh
@@ -2,9 +2,11 @@
 export SCOOP="/opt/scoop"
 export SCOOP_HOME="/opt/scoop"
 export SCOOP_CACHE="/opt/cache"
+rm /opt/cache/*
 cd /opt/scoop
 powershell ./bin/auto-pr.ps1 -p -s ngrok,curl
 cd /opt/scoop-extras
 powershell ./bin/auto-pr.ps1 -p
 cd /opt/scoop-versions
 powershell ./bin/auto-pr.ps1 -p
+rm /opt/cache/*
