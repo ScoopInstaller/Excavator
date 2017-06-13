a6a44d2d63bac729b8d0d84e6546b371934c3910
Richard Kuhnt <r15ch13+git@gmail.com>
2017-06-13 20:59:43 +0200

    Fix git environment variables

--- excavate.sh
+++ excavate.sh
@@ -2,8 +2,8 @@
 export SCOOP="/opt/scoop"
 export SCOOP_HOME="/opt/scoop"
 export SCOOP_CACHE="/opt/cache"
-git config --global user.name "$GIT_USERNAME"
-git config --global user.email "$GIT_EMAIL"
+git config --global user.name "`cat /etc/container_environment/GIT_USERNAME`"
+git config --global user.email "`cat /etc/container_environment/GIT_EMAIL`"
 rm /opt/cache/*
 cd /opt/scoop
 powershell ./bin/auto-pr.ps1 -p -s ngrok,curl
